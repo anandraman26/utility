628a67aece455dd84734eea3cc8209724981f7c7
Novelvox <Novelvox@host.docker.internal>
2021-01-18 11:51:18 +0530

    syso

--- src/com/nv/hclutility/HCLUtilityEmail.java
+++ src/com/nv/hclutility/HCLUtilityEmail.java
@@ -27,7 +27,7 @@ public class HCLUtilityEmail {
 		String flag = PropertyUtil.getInstance().getValueForKey("hclutility.thread.flag");
 		if (flag.equals("true")) {
 			try {
-				System.out.println("Commit");
+				System.out.println("Commit initial");
 				new Thread(EmailThread.getInstance()).start();
 			} catch (Exception e) {
 				LOGGER.info("Exception in Email Thread ", e);
